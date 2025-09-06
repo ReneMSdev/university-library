d3989dda4c93ec4ebf87110498f7d085eb74580d
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-09-06 18:04:42 -0500

    Book Cover

---
+++ components/BookCover.tsx
@@ -0,0 +1,44 @@
+import { cn } from '../lib/utils'
+import Image from 'next/image'
+
+type BookCoverVarient = 'extraSmall' | 'small' | 'medium' | 'regular' | 'wide'
+
+const variantStyles: Record<BookCoverVarient, string> = {
+  extraSmall: 'book-cover_extra_small',
+  small: 'book-cover_small',
+  medium: 'book-cover_medium',
+  regular: 'book-cover_regular',
+  wide: 'book-cover_wide',
+}
+
+interface Props {
+  className?: string
+  variant?: BookCoverVarient
+  coverColor: string
+  coverUrl: string
+}
+
+const BookCover = ({
+  className,
+  variant = 'regular',
+  coverColor = '#012B48',
+  coverImage = 'https://placehold.co/400x600',
+}: Props) => {
+  return (
+    <div className={cn('relative transition-all duration-300', variantStyles[variant], className)}>
+      BOOK SIDE SVG
+      <div
+        className='absoute z-10'
+        style={{ left: '12%', width: '87.5%', height: '88%' }}
+      >
+        <Image
+          src={coverImage}
+          alt='Book cover'
+          fill
+          className='rounded-sm object-fill'
+        />
+      </div>
+    </div>
+  )
+}
+export default BookCover
